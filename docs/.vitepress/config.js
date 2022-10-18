0c18197525803601bff4149eef2a387fa41a4cb9
纾浚 <Junical@email.cn>
2022-10-18 19:57:04 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     themeConfig:{
         siteTitle:'曙前杂记',
         footer: {
-            message: '本站文章禁止转载',
-            copyright: '版权所有 © 2022 纾浚'
+            message: '琼ICP备2022001156号-2',
+            copyright: '本站文章禁止转载 | 版权所有 © 2022 纾浚'
         },
         docFooter: {
             prev: '上一篇',
